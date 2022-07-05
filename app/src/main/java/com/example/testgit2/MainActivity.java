82051702acfd84cd65b5eb63560a96f59a7ece0e
LOLScrewer <caozhiyuan1988@163.com>
2022-07-06 00:40:47 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/testgit2/MainActivity.java
+++ app/src/main/java/com/example/testgit2/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //阿迪斯发士大夫撒
     }
 }
